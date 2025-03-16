0ac6c05ec21eb729d2fd13cb1eea1cc49788511a
mathew rym <rymmathew@gmail.com>
2025-03-17 00:15:51 +0300

    Dodger added

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+
+function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+      console.log('Button clicked!');
+    });
+  }
+  
+  function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left) || 0;
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left) || 0;
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  module.exports = { addingEventListener, moveDodgerLeft, moveDodgerRight };
